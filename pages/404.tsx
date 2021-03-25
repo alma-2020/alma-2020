1a64e7cbf7c65b23705199d3a9946521c7bd40d9
alma-2020 <studiosjakku@gmail.com>
2021-03-25 16:50:41 -0300

    fix our 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,8 +2,8 @@ import Layout from '../components/layout'
 
 export default function Custom404() {
     return (
-        <Layout>
-            <h1>404 - Page Not Found</h1>
-        </Layout>
+        <h1 style={{ textAlign: 'center' }}>
+            404 - Page Not Found
+        </h1>
     );
 }
